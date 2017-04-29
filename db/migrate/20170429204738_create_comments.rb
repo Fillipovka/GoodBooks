7548c0caf8ac909c98025b00ad91867053ace83c
Fillipovka <tamara_novik@yahoo.com>
2017-04-29 23:48:53 +0300

    Update

--- db/migrate/20170429204738_create_comments.rb
+++ db/migrate/20170429204738_create_comments.rb
@@ -1,10 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text :content
-      t.integer :commentable_id
-      t.string :commentable_type
-
+    	t.text :comment_text
+      t.belongs_to :commentable, polymorphic: true
       t.timestamps
     end
     add_index :comments, [:commentable_id, :commentable_type]
