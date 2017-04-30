836376ec2695923ffd4c67d4f06706ce68f94824
Fillipovka <tamara_novik@yahoo.com>
2017-04-30 16:00:56 +0300

    Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
-	before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-	def index
+  def index
     @articles = if params[:tag]
       Article.tagged_with(params[:tag])
     else
@@ -9,26 +9,26 @@ class ArticlesController < ApplicationController
     end
   end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@article = Article.new
-	end
+  def new
+    @article = Article.new
+  end
 
-	def create
-		@article = Article.new(article_params)
+  def create
+    @article = Article.new(article_params)
     if @article.save
       redirect_to @article
     else
       render 'new'
     end
-	end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
+  def update
     if @article.update_attributes(article_params)
       redirect_to @article
     else
@@ -41,13 +41,13 @@ class ArticlesController < ApplicationController
     redirect_to article_url
   end
 
-	private
-		def set_article
-			@article = Article.find(params[:id])
-		end
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-		def article_params
-			params.require(:article).permit(:title_article, :text_article, :photo_article, :tag_list,
-																			:category_ids => [], :book_ids => [])
-		end
+    def article_params
+      params.require(:article).permit(:title_article, :text_article, :photo_article, :tag_list,
+                                      :category_ids => [], :book_ids => [])
+    end
 end
