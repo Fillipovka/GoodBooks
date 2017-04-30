0efa6be02a69b99aa5a5028dc8aa81d7125bfebe
Fillipovka <tamara_novik@yahoo.com>
2017-04-30 16:02:47 +0300

    Update

--- app/controllers/favorite_books_controller.rb
+++ app/controllers/favorite_books_controller.rb
@@ -1,16 +1,16 @@
 class FavoriteBooksController < ApplicationController
-	before_action :logged_in_user
+  before_action :logged_in_user
 
   def create
-		@book = Book.find(params[:book_id])
-		current_user.favorite(@book)
-		respond_to do |format|
+    @book = Book.find(params[:book_id])
+    current_user.favorite(@book)
+    respond_to do |format|
       format.html { redirect_to @book }
       format.js
     end
-	end
+  end
 
-	def destroy
+  def destroy
     @book = FavoriteBook.find(params[:id]).book
     current_user.unfavorite(@book)
     respond_to do |format|
