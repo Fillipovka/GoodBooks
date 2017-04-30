6aa8c7037533bee945626f7cd85fecdb70a8ea99
Fillipovka <tamara_novik@yahoo.com>
2017-05-01 00:31:36 +0300

    Update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,14 @@
 //= require_tree .
 
 $(function() {
+   $('ul.nav li.dropdown').hover(function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+   }, function() {
+      $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+   });
+});
+
+$(function() {
    $(".input input").focus(function() {
 
       $(this).parent(".input").each(function() {
