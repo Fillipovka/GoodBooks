e45c35947a64d70efaa2692f245d1d9e71edb894
Fillipovka <tamara_novik@yahoo.com>
2017-05-02 01:40:44 +0300

    Update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   def index
     @home_page = true if logged_in?
     @last_articles = Article.last(4)
-    @tags = Tag.last(40)
+    @tags = Tag.last(68)
   end
 end
